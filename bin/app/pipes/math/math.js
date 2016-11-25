7988c7c001946acf1aa2c521804bfc07a901c965
danrevah <drevah@outbrain.com>
2016-11-25 11:14:31 +0200

    release

--- bin/app/pipes/math/math.js
+++ bin/app/pipes/math/math.js
@@ -7,8 +7,10 @@ var floor_1 = require("./floor");
 var round_1 = require("./round");
 var sqrt_1 = require("./sqrt");
 var pow_1 = require("./pow");
+var ceil_1 = require("./ceil");
 exports.MATH_PIPES = [
-    max_1.MaxPipe, min_1.MinPipe, percent_1.PercentPipe, sum_1.SumPipe, floor_1.FloorPipe, round_1.RoundPipe, sqrt_1.SqrtPipe, pow_1.PowerPipe
+    max_1.MaxPipe, min_1.MinPipe, percent_1.PercentPipe, sum_1.SumPipe, floor_1.FloorPipe,
+    round_1.RoundPipe, sqrt_1.SqrtPipe, pow_1.PowerPipe, ceil_1.CeilPipe
 ];
 
 //# sourceMappingURL=math.js.map
