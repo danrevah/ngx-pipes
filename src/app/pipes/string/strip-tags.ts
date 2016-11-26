5c35fc332226d89f024e28a9abd60b41560c83e0
danrevah <drevah@outbrain.com>
2016-11-26 19:52:36 +0200

    bump

--- src/app/pipes/string/strip-tags.ts
+++ src/app/pipes/string/strip-tags.ts
@@ -4,7 +4,7 @@ import {PipeTransform, Pipe, Injectable} from '@angular/core';
 @Pipe({name: 'stripTags'})
 export class StripTagsPipe implements PipeTransform {
 
-  transform(text:string, ...allowedTags: any[] = []):string {
+  transform(text:string, ...allowedTags: any[]):string {
     return allowedTags.length > 0
         ? text.replace(new RegExp(`<(?!\/?(${allowedTags.join('|')})\s*\/?)[^>]+>`, 'g'), '')
         : text.replace(/<(?:.|\s)*?>/g, '');
