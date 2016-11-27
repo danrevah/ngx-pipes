67a477ab90bd588aaddd25ad0e859bbbb31912e6
danrevah <drevah@outbrain.com>
2016-11-27 18:28:04 +0200

    get rid of leftovers

--- bin/main.js
+++ bin/main.js
@@ -1,12 +1,4 @@
 "use strict";
 require('./polyfills.ts');
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var core_1 = require('@angular/core');
-var environment_1 = require('./environments/environment');
-var _1 = require('./app/');
-if (environment_1.environment.production) {
-    core_1.enableProdMode();
-}
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(_1.AppModule);
 
 //# sourceMappingURL=main.js.map
