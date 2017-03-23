c733a3e5dc9aad624a2a1d65c45e36999e7c7eed
Sarah Ahmed <sarahsga4@gmail.com>
2017-03-23 15:10:28 +0500

    remove forced execution of my tests

--- src/app/pipes/string/insert.spec.ts
+++ src/app/pipes/string/insert.spec.ts
@@ -7,18 +7,18 @@ describe('InsertPipe', () => {
     pipe = new InsertPipe();
   });
 
-  fit('Should not do anything if text is not a string', () => {
+  it('Should not do anything if text is not a string', () => {
     expect(pipe.transform(null)).toEqual(null);
     expect(pipe.transform(undefined)).toEqual(undefined);
     expect(pipe.transform(42)).toEqual(42);
     expect(pipe.transform({name: 'foo'})).toEqual({name: 'foo'});
   });
 
-  fit('should insert chars properly after every character', () => {
+  it('should insert chars properly after every character', () => {
     expect(pipe.transform('foobar', '.')).toEqual('f.o.o.b.a.r');
   });
 
-  fit('should do nothing if no chars parameter is passed', () => {
+  it('should do nothing if no chars parameter is passed', () => {
     expect(pipe.transform('foobar')).toEqual('foobar');
   });
 
