bfe0fef2647c4ba3c8bb7d596267f0d36f21daf6
Sarah Ahmed <sarahsga4@gmail.com>
2017-03-23 16:21:48 +0500

    feat(String-Insert): Add step size option to insert pipe

--- src/app/pipes/string/insert.spec.ts
+++ src/app/pipes/string/insert.spec.ts
@@ -1,7 +1,7 @@
 import { InsertPipe } from './insert';
 
 describe('InsertPipe', () => {
-  let pipe:InsertPipe;
+  let pipe: InsertPipe;
 
   beforeEach(() => {
     pipe = new InsertPipe();
@@ -11,15 +11,20 @@ describe('InsertPipe', () => {
     expect(pipe.transform(null)).toEqual(null);
     expect(pipe.transform(undefined)).toEqual(undefined);
     expect(pipe.transform(42)).toEqual(42);
-    expect(pipe.transform({name: 'foo'})).toEqual({name: 'foo'});
+    expect(pipe.transform({ name: 'foo' })).toEqual({ name: 'foo' });
   });
 
-  it('should insert chars properly after every character', () => {
-    expect(pipe.transform('foobar', '.')).toEqual('f.o.o.b.a.r');
+  it('Should use stepSize of 1 if stepSize is not a number', () => {
+    expect(pipe.transform('foobar', '-', 'hello')).toEqual('f-o-o-b-a-r');
   });
 
   it('should do nothing if no chars parameter is passed', () => {
     expect(pipe.transform('foobar')).toEqual('foobar');
+  });
+
+  it('should insert chars properly', () => {
+    expect(pipe.transform('foobar', '.')).toEqual('f.o.o.b.a.r');
+    expect(pipe.transform('foobar with stepsize', '#', 3)).toEqual('foo#bar# wi#th #ste#psi#ze');
   });
 
 });
--- src/app/pipes/string/insert.ts
+++ src/app/pipes/string/insert.ts
@@ -1,16 +1,28 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import {isString} from '../helpers/helpers';
+import { isString } from '../helpers/helpers';
+import { isNumber } from '../helpers/helpers';
 
 @Pipe({
   name: 'insert'
 })
 export class InsertPipe implements PipeTransform {
 
-  transform(text: any, chars: string = ''): any {
+  transform(text: any, chars: string = '', stepSize: number = 1): any {
+    function chunk(): string[] {
+
+      if (!isNumber(stepSize)) {
+        return text.split('');
+      }
+
+      let transformedArr = [];
+      for (let i = 0; i < text.length; i += stepSize) {
+        transformedArr.push(text.substr(i, stepSize))
+      }
+      return transformedArr;
+    }
 
     return isString(text)
-      ? text.trim()
-        .split('')
+      ? chunk()
         .join(chars)
       : text;
 
