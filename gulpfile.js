e60de786d43afa1815959aa386eee611b4c44869
danrevah <drevah@outbrain.com>
2017-04-05 00:17:41 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const clean = require('gulp-clean');
 
 gulp.task('clean', () => {
   return gulp.src([
-    './src/**/*.d.ts', './src/**/*.js', './src/**/*.js.map', './src/**/*.metadata.json',
+    'esm', 'bundles', './src/**/*.d.ts', './src/**/*.js',
+    './src/**/*.js.map', './src/**/*.metadata.json',
     './src/**/*.ngsummary.json','./src/**/*.ngfactory.ts'
   ]).pipe(clean());
 });
