ef02b70c7e5fe39311015abe810001fb378a0223
Islam Adel Omar <iarayan@live.com>
2017-06-01 13:34:06 +0200

    Enhancement/ Unique filter array based on object property (#58)
    * Enhancement/ Unique filter array based on object property
    
    * Enhancement/ Using helper functions (isUndefined, isObject, extractDeepPropertyByMapKey), supporting deep-properties unique values even when a deep-property is pointing to an object.

--- src/app/pipes/array/unique.spec.ts
+++ src/app/pipes/array/unique.spec.ts
@@ -22,4 +22,17 @@ describe('DiffPipe', () => {
     expect(pipe.transform([1, 2, 3, 1, 2, 3])).toEqual([1, 2, 3]);
     expect(pipe.transform([1, 2, 5, 1, 3, 7, 8, 5, 10])).toEqual([1,2,5,3,7,8,10]);
   });
+
+  it('should filter the array based on object properties when a property name is supplied in arguments',()=>{
+  	expect(pipe.transform([{a:1,b:true},{a:1,b:true},{a:3,b:false}],'b')).toEqual([{a:1,b:true},{a:3,b:false}]);
+  	expect(pipe.transform([1,2,{a:1,b:true},{a:3,b:false}],'b')).toEqual([{a:1,b:true},{a:3,b:false}]);
+  	expect(pipe.transform([1,2,{a:1,b:true},{a:3,b:false}],'c')).toEqual([]);    
+  })
+  
+  it('should filter the array based on object deep properties when a property name dot separated is supplied in arguments',()=>{
+  	expect(pipe.transform([{b:{c:{d:true}}},{b:{c:{d:false}}},{b:{c:{d:true}}}],'b.c.d')).toEqual([{b:{c:{d:true}}},{b:{c:{d:false}}}]);
+    expect(pipe.transform([1,2,{b:{c:{d:true}}},{b:{c:{d:false}}}],'b.c.d')).toEqual([{b:{c:{d:true}}},{b:{c:{d:false}}}]);
+  	expect(pipe.transform([{b:{c:{d:true}}},{b:{c:{d:false}}}],'b.c')).toEqual([{b:{c:{d:true}}},{b:{c:{d:false}}}]);
+  	expect(pipe.transform([{b:{c:{d:true}}},{b:{c:{d:false}}}],'b.c.e')).toEqual([]);
+  })
 });
--- src/app/pipes/array/unique.ts
+++ src/app/pipes/array/unique.ts
@@ -1,11 +1,24 @@
-import {PipeTransform, Pipe} from '@angular/core';
+import { PipeTransform, Pipe } from '@angular/core';
+import { isUndefined, isObject, extractDeepPropertyByMapKey } from  '../helpers/helpers';
 
-@Pipe({name: 'unique'})
+@Pipe({ name: 'unique' })
 export class UniquePipe implements PipeTransform {
 
-  transform(input: any): any[] {
-    return Array.isArray(input)
-      ? input.filter((e, i) => input.indexOf(e) === i)
-      : input;
+  transform(input: any, propertyName?: string): any[] {
+    const uniques: boolean[] = [];
+    return Array.isArray(input) ?
+      isUndefined(propertyName) ?
+        input.filter((e, i) => input.indexOf(e) === i) :
+        input.filter((e, i) => {
+          let value = extractDeepPropertyByMapKey(e,propertyName);
+          value = isObject(value)? JSON.stringify(value):value;
+
+          if (isUndefined(value) || uniques[value]) {
+            return false;
+          }
+
+          uniques[value] = true;
+          return true;
+        }) : input;
   }
 }
