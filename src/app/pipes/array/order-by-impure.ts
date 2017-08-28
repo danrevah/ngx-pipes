e5e01918fef63b4d6fac97287ad951bdd1f6e89b
drevah <drevah@outbrain.com>
2017-08-29 01:57:25 +0300

    Order by impure

--- src/app/pipes/array/order-by-impure.ts
+++ src/app/pipes/array/order-by-impure.ts
@@ -1,5 +1,5 @@
 import {Pipe} from '@angular/core';
 import {OrderByPipe} from './order-by';
 
-@Pipe({name: 'orderByImpure'})
+@Pipe({name: 'orderByImpure', pure: false})
 export class OrderByImpurePipe extends OrderByPipe {}
