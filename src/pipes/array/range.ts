1b9d07f91db6f2dfd77b6bf2d24bc12a1b042ca1
Albertino Júnior <jr.albertino@outlook.com>
2017-11-09 21:18:30 -0200

    arrow function

--- src/pipes/array/range.ts
+++ src/pipes/array/range.ts
@@ -3,11 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'range' })
 export class RangePipe implements PipeTransform {
   transform(start: number = 1, count: number = 0, step: number = 1): any {
-    let next;
-    return Array(count).fill(null).map(function (v, i) {
-      next = start;
-      start += step;
-      return next;
-    });
+    return Array(count).fill('').map((v, i) => step * i + start);
   }
 }
