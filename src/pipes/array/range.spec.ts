6b750e815a98868103eb30c01167d79dbe2f0dca
Albertino Júnior <jr.albertino@outlook.com>
2017-11-08 23:38:37 -0200

    execute tests

--- src/pipes/array/range.spec.ts
+++ src/pipes/array/range.spec.ts
@@ -1,32 +1,51 @@
 import { RangePipe } from './range';
 
-describe('RangePipe', () => {
+fdescribe('RangePipe', () => {
   let pipe: RangePipe;
 
   beforeEach(() => {
     pipe = new RangePipe();
   });
 
-  it('should return the input if not an array', () => {
-    expect(pipe.transform(true)).toEqual(true);
-    expect(pipe.transform(10)).toEqual(10);
+  it('should keep the empty array way if it doesn\'t have some of the arguments', () => {
+    expect(pipe.transform()).toEqual([]);
+
+    expect(pipe.transform(0)).toEqual([]);
+    expect(pipe.transform(1)).toEqual([]);
+
+    expect(pipe.transform(0, 0)).toEqual([]);
   });
 
-  it('should keep the array the same way if it doesn\'t have any without arguments', () => {
-    expect(pipe.transform([1, 2, 3])).toEqual([1, 2, 3]);
-    expect(pipe.transform([])).toEqual([]);
+  it('should returns a new array with the length of the second argument', () => {
+    expect(pipe.transform(0, 0)).length == 0;
+    expect(pipe.transform(0, 1000)).length == 1000;
+    expect(pipe.transform(1, 5)).length == 5;
+
+    expect(pipe.transform(0, 0, 0)).length == 0;
+    expect(pipe.transform(0, 0, 2)).length == 0;
+    expect(pipe.transform(1, 5, 0)).length == 5;
+    expect(pipe.transform(1, 5, 2)).length == 5;
   });
 
   it('should returns a new array filled', () => {
-    expect(pipe.transform([], 0, 5)).toEqual([0, 1, 2, 3, 4]);
-    expect(pipe.transform([], 1, 5)).toEqual([1, 2, 3, 4, 5]);
+    expect(pipe.transform(0, 5)).toEqual([0, 1, 2, 3, 4]);
+    expect(pipe.transform(1, 5)).toEqual([1, 2, 3, 4, 5]);
 
-    expect(pipe.transform([], 1, 5, 1)).toEqual([1, 2, 3, 4, 5]);
-    expect(pipe.transform([], 1, 5, 2)).toEqual([1, 3, 5, 7, 9]);
-    expect(pipe.transform([], 2, 5, 2)).toEqual([2, 4, 6, 8, 10]);
+    expect(pipe.transform(1, 5, 0)).toEqual([1, 1, 1, 1, 1]);
+    expect(pipe.transform(1, 5, 1)).toEqual([1, 2, 3, 4, 5]);
+    expect(pipe.transform(1, 5, 2)).toEqual([1, 3, 5, 7, 9]);
+    expect(pipe.transform(2, 5, 2)).toEqual([2, 4, 6, 8, 10]);
+  });
 
-    expect(pipe.transform([8, 9], 0, 5)).toEqual([8, 9, 0, 1, 2, 3, 4]);
-    expect(pipe.transform([8, 9], 1, 5)).toEqual([8, 9, 1, 2, 3, 4, 5]);
+  it('should returns a new array filled with only negatives', () => {
+    expect(pipe.transform(0, 5, -1)).toEqual([0, -1, -2, -3, -4]);
+    expect(pipe.transform(-1, 5, -2)).toEqual([-1, -3, -5, -7, -9]);
+    expect(pipe.transform(-2, 5, -2)).toEqual([-2, -4, -6, -8, -10]);
+  });
+
+  it('should return a new array filled with range from -5 to +5', () => {
+    expect(pipe.transform(-5, 11)).toEqual([-5, -4, -3, - 2, -1, 0, 1, 2, 3, 4, 5]);
+    expect(pipe.transform(-5, 11, 1)).toEqual([-5, -4, -3, - 2, -1, 0, 1, 2, 3, 4, 5]);
   });
 
 });
--- src/pipes/array/range.ts
+++ src/pipes/array/range.ts
@@ -2,21 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'range' })
 export class RangePipe implements PipeTransform {
-  transform(input: any, start: number = 1, count: number = 0, step: number = 1): any {
-
-    if (!Array.isArray(input)) {
-      return input;
-    }
-
-    // return input.concat(Array(count).fill('').map((x, i) => (i + start)));
-
-    let range: number[] = [];
-
-    for (let length = 0; length < count; ++length) {
-      range.push(start);
+  transform(start: number = 1, count: number = 0, step: number = 1): any {
+    let next;
+    return Array(count).fill(null).map(function (v, i) {
+      next = start;
       start += step;
-    }
-
-    return range;
+      return next;
+    });
   }
 }
