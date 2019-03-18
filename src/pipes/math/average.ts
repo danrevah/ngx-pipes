6407bac9d3ca21045c294afccc56accafe871fb4
danrevah <danrevah89@gmail.com>
2019-03-18 16:00:10 +0200

    average pipe

---
+++ src/pipes/math/average.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {isNumberFinite} from '../helpers/helpers';
+
+@Pipe({ name: 'average' })
+export class AveragePipe implements PipeTransform {
+
+  transform(arr: number[]): string | number {
+    const isValid = arr.every(value => isNumberFinite(value));
+
+    if (!isValid) {
+      return NaN;
+    }
+
+    return arr.length
+      ? arr.reduce((sum, value) => sum + value, 0) / arr.length
+      : 0;
+  }
+}
