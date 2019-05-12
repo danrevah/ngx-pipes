fc0146a98f088bc6ee999244a81d82349a4640df
danrevah <danrevah89@gmail.com>
2019-05-12 21:41:35 +0300

    feat(): from pairs pipe

---
+++ src/pipes/array/from-pairs.spec.ts
@@ -0,0 +1,24 @@
+import {FromPairsPipe} from './from-pairs';
+
+describe('FromPairsPipe', () => {
+  let pipe: FromPairsPipe;
+
+  beforeEach(() => {
+    pipe = new FromPairsPipe();
+  });
+
+  it('should not do anything with non-arrays', () => {
+    expect(pipe.transform(42)).toBe(42);
+    expect(pipe.transform('foo')).toBe('foo');
+    expect(pipe.transform(undefined)).toBe(undefined);
+  });
+
+  it('should transform array to object', () => {
+    expect(pipe.transform([['foo', 42]])).toEqual({ foo: 42 });
+    expect(pipe.transform([['foo', 42], ['bar', 'foobar']])).toEqual({ foo: 42, bar: 'foobar' });
+  });
+
+  it('should ignore non-array elements', () => {
+    expect(pipe.transform([['foo', 42], 1, ['bar', null], 'baz'])).toEqual({ foo: 42, bar: null });
+  });
+});
---
+++ src/pipes/array/from-pairs.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'fromPairs' })
+export class FromPairsPipe implements PipeTransform {
+
+  transform(input: any): any {
+    if (!Array.isArray(input)) {
+      return input;
+    }
+
+    return input.reduce((obj, arr) => {
+      if (!Array.isArray(arr)) {
+        return obj;
+      }
+
+      const [prop, val] = arr;
+
+      obj[prop] = val;
+
+      return obj;
+    }, {});
+  }
+}
